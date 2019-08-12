24ef3a8f3107dc7254f626229b7490d225aabe6c
Orlan-Asuncion <48038688+Orlan-Asuncion@users.noreply.github.com>
2019-08-12 18:45:41 -0400

    updated files

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 const Footer = () => (
 	<footer className="footer">
 		<div className="footer-bottom">
-			Clicky Game App 
+			Clicky Game App(React)
 			<img alt="react" className="ml-2 mb-1" src="assets/images/react-logo.jpg"/>
 		</div>
 	</footer>
